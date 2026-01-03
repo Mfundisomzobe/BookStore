2a409eb0aa2a70fc165b0648b7bef6222b8eb7d7
Mzobe, Mfundiso, (Mr) (s225843994) <s225843994@mandela.ac.za>
2026-01-03 20:50:41 +0200

    first commit

--- UmarWeb/Program.cs
+++ UmarWeb/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
